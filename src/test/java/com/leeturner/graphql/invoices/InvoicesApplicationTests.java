be056419f64dc8676f4bc4dba44215566edf7a64
Lee Turner <lee@leeturner.me>
2019-07-27 19:01:53 +0100

    Code reformat

--- src/test/java/com/leeturner/graphql/invoices/InvoicesApplicationTests.java
+++ src/test/java/com/leeturner/graphql/invoices/InvoicesApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class InvoicesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
