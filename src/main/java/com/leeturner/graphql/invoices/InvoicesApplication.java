f9e1cfbfb26d61d50374d923b86dc21fd4007562
Lee Turner <lee@leeturner.me>
2019-07-28 22:41:15 +0100

    New data added to seed the db

--- src/main/java/com/leeturner/graphql/invoices/InvoicesApplication.java
+++ src/main/java/com/leeturner/graphql/invoices/InvoicesApplication.java
@@ -1,5 +1,7 @@
 package com.leeturner.graphql.invoices;
 
+import java.util.List;
+
 import com.leeturner.graphql.invoices.model.Client;
 import com.leeturner.graphql.invoices.model.Invoice;
 import com.leeturner.graphql.invoices.repository.ClientRepository;
@@ -19,11 +21,16 @@ public class InvoicesApplication {
     @Bean
     public CommandLineRunner invoices(InvoiceRepository invoiceRepository, ClientRepository clientRepository) {
         return (args) -> {
-            Client client = new Client("Bobs Marketing Agency", 28, "23 Brighton Street", "Rottingdean", "Brighton", "BN2 7DP");
-            clientRepository.save(client);
+            // seed the database with some example data so we can have a play with the GraphQL queries
+            Client bobsMarketingAgency = new Client("Bobs Marketing Agency", 28, "23 Brighton Street", "Rottingdean", "Brighton", "BN2 7DP");
+            Invoice bobsInvoice1 = new Invoice("DRAFT", null, "GBP", 120.00, 100.00, 20.00, bobsMarketingAgency);
+            Invoice bobsInvoice2 = new Invoice("ISSUED", "2019-07-28", "GBP", 1200.00, 1000.00, 200.0, bobsMarketingAgency);
 
-            Invoice invoice = new Invoice("DRAFT", null, "GBP", 120.00, 100.00, 20.00, client);
-            invoiceRepository.save(invoice);
+            Client jillsAccountancyCompany = new Client("Jills Accountancy Company", 28, "24 Eastbourne Rd", null, "Eastboaurne", "BN23 5GP");
+            Invoice jillsInvoice1 = new Invoice("DRAFT", null, "GBP", 120.00, 100.00, 20.00, jillsAccountancyCompany);
+
+            clientRepository.saveAll(List.of(bobsMarketingAgency,jillsAccountancyCompany));
+            invoiceRepository.saveAll(List.of(bobsInvoice1, bobsInvoice2, jillsInvoice1));
         };
     }
 
