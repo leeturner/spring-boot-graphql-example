e4dc0609bc20c00886fb2a8dd2107c67ff98bcdd
Lee Turner <lee@leeturner.me>
2019-08-01 22:38:54 +0100

    Adding junit 5 and updating test to use jupiter imports

--- src/test/java/com/leeturner/graphql/invoices/InvoicesApplicationTests.java
+++ src/test/java/com/leeturner/graphql/invoices/InvoicesApplicationTests.java
@@ -1,16 +1,16 @@
 package com.leeturner.graphql.invoices;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class InvoicesApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+class InvoicesApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
