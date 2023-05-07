531b1c966775dcfc55af1d2368aac5f638fea6da
leeturner <lee@leeturner.me>
2023-05-07 10:09:49 +0100

    Update to spring boot 3.0.6

--- src/main/java/com/leeturner/graphql/invoices/model/Invoice.kt
+++ src/main/java/com/leeturner/graphql/invoices/model/Invoice.kt
@@ -1,6 +1,11 @@
 package com.leeturner.graphql.invoices.model
 
-import javax.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 
 @Entity
 data class Invoice(
